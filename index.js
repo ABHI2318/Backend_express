fc25549635f0a343b602db4757584192fb6fe940
abhishek <abhisheksingh961931@gmail.com>
2024-07-21 13:45:35 +0530

    sums

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ const express=require('express');
 const port=3000;
 const app=express();
 
+app.get("/routehandler",function(req,res){
+    res.json({
+        message: "This is a route handler",
+        names: ["John", "Doe", "Jane"]
+    });
+});
+
+app.post('/conversations',(req,res)=>{
+    console.log(req.headers);
+    res.send({
+        message: 'You made a POST request to /conversations',
+    })
+})
+
 app.get('/',function(req,res){
     res.send('hello to express')
 
 })
 
-app.listen(port)
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
