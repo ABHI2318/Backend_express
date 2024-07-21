39304562ba1dd5a6dd3dfd0a3d71bd4fc1bed484
abhishek <abhisheksingh961931@gmail.com>
2024-07-21 20:56:37 +0530

    4 reqs done

--- hospital_backend.js
+++ hospital_backend.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 
 var users = [{
-    name: 'John Doe',
+    name: ['John Doe','abishek'],
     kidneys: [{
         healthy: false
     }]
+   
 }];
+
+app.use(express.json());
 
 app.get('/', function(req, res) {
     const johnkidneys = users[0].kidneys;
@@ -25,6 +28,32 @@ app.get('/', function(req, res) {
         noofunhealthykidneys: noofkidneys - noofhealthykidneys
     });
 });
+
+
+app.post('/',function(req, res) {
+    const ishealthy=req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: ishealthy
+    })
+
+    res.json({
+        message:"done sucessfully"
+    })
+})
+
+app.put('/', function(req, res) {
+    for(let i=0;i<users[0].kidneys.length;i++){
+        users[0].kidneys[i].healthy=true;
+    }
+    res.json({})
+
+})
+
+app.delete('/', function(req, res) {
+    users[0].kidneys=[];
+    res.json({})
+})
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
